de5af67badac10a54f6fdc51ee0ae09d16f9f198
olegbucolo <olegbucolo97@gmail.com>
2026-05-19 17:37:01 +0200

    START back for doctor booking system. ADD Dockerfile, docker-compose

---
+++ src/main/java/org/doctors/docp/DocpApplication.java
@@ -0,0 +1,13 @@
+package org.doctors.docp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/doctors/docp/DocpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.doctors.docp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
